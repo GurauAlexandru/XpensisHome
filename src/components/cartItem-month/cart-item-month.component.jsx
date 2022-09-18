7a6d4b374139803da81e5a40107b3dc8bacbf52b
Alexandru-Gurau <alexandru.gurau@live.com>
2022-09-18 22:27:35 +0300

    fixind rerender months and years arrays

--- src/components/cartItem-month/cart-item-month.component.jsx
+++ src/components/cartItem-month/cart-item-month.component.jsx
@@ -2,11 +2,25 @@ import './cart-item-month.styles.scss';
 
 import { useContext } from 'react';
 import { UserContext } from '../../context/user.context';
+import { useEffect } from 'react';
 
 const CartItemMonth = () => {
-  const { setMonth, setYear, accountDetails } = useContext(UserContext);
+  const { setMonth, setYear, accountDetails, month, year } =
+    useContext(UserContext);
 
   const { allMonths, allYears } = accountDetails();
+
+  useEffect(() => {
+    const monthIndex = allMonths.findIndex((el) => el === month);
+    const selectMonth = document.querySelector('.month-options');
+    selectMonth.querySelectorAll('option')[monthIndex].selected = 'selected';
+  }, [month, allMonths]);
+
+  useEffect(() => {
+    const yearIndex = allYears.findIndex((el) => el === year.toString());
+    const selectYear = document.querySelector('.year-options');
+    selectYear.querySelectorAll('option')[yearIndex].selected = 'selected';
+  }, [year, allYears]);
 
   const handleSelectMonth = (e) => {
     setMonth(e.target.value);
@@ -20,26 +34,20 @@ const CartItemMonth = () => {
     <div className='cart-item__month'>
       <h3 className='header h-small'>
         <select className='month-options' onChange={handleSelectMonth}>
-          {allMonths
-            ? allMonths.map((el) => (
-                <option key={el} value={el}>
-                  {el}
-                </option>
-              ))
-            : allMonths[0]}
+          {allMonths.map((el) => (
+            <option key={el} value={el}>
+              {el}
+            </option>
+          ))}
         </select>
       </h3>
       <h3 className='header h-cart-huge'>
         <select className='year-options' onChange={handleSelectYear}>
-          {allYears
-            .map((el) => {
-              return (
-                <option key={el} value={el}>
-                  {el}
-                </option>
-              );
-            })
-            .reverse()}
+          {allYears.map((el) => (
+            <option key={el} value={el}>
+              {el}
+            </option>
+          ))}
         </select>
       </h3>
     </div>
