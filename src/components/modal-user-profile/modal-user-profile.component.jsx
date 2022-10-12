a4adfdb39a243a9f5fbbb18b8b93f88d5db336f9
Alexandru-Gurau <alexandru.gurau@live.com>
2022-10-12 21:49:25 +0300

    adding a switch for accot

--- src/components/modal-user-profile/modal-user-profile.component.jsx
+++ src/components/modal-user-profile/modal-user-profile.component.jsx
@@ -1,14 +1,27 @@
 import './modal-user-profile.styles.scss';
 
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
 import { UserContext } from '../../context/user.context';
 import ImageProfile from '../image-profile/image-profile.component';
+import Buton from '../button/button.component';
 
 const ModalUserProfile = () => {
   const { currentUser } = useContext(UserContext);
+  const [classAccount, setClassAccount] = useState(true);
+  const [classSecurity, setClassSecurity] = useState(false);
+  const [container, setContainer] = useState(true);
 
-  console.log(currentUser);
+  const handleClickAccount = () => {
+    setContainer(true);
+    setClassAccount(true);
+    setClassSecurity(false);
+  };
+  const handleClickSecurity = () => {
+    setContainer(false);
+    setClassAccount(false);
+    setClassSecurity(true);
+  };
 
   return (
     <section className='user-profile-page'>
@@ -23,8 +36,33 @@ const ModalUserProfile = () => {
           <ImageProfile user={currentUser} />
           <h3 className='header h-large'>{currentUser.name}</h3>
           <p className='p-cart-action'>{currentUser.email}</p>
+
+          <Buton
+            className={`button button__text  mt-large ${
+              classAccount ? 'button__text--active' : ''
+            }`}
+            onClick={() => handleClickAccount()}
+          >
+            Account
+          </Buton>
+          <Buton
+            className={`button button__text ${
+              classSecurity ? 'button__text--active' : ''
+            }`}
+            onClick={() => handleClickSecurity()}
+          >
+            Security
+          </Buton>
         </div>
-        <div className='user-profile-page__container--content'></div>
+        {container ? (
+          <div className='user-profile-page__container--account'>
+            <h2 className='header h-xLarge'>Account settings</h2>
+          </div>
+        ) : (
+          <div className='user-profile-page__container--security'>
+            <h2 className='header h-xLarge'>Security settings</h2>
+          </div>
+        )}
       </div>
     </section>
   );
